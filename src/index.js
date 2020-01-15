f6748e6f57bf600fc0c71dd0bf575f5b238b74db
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-01-14 23:12:26 -0300

    finish backend

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+mongoose.connect(
+  'mongodb+srv://kasio:88222547@devfinder-vqqf3.mongodb.net/devfinder?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
