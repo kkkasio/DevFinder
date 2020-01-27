7660d12043bbe33079d72007c510b58b6b0b7ff7
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-01-27 18:46:28 -0300

    fix

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,9 @@ const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  res.status(200).send({ message: 'Hello world! :)' });
+});
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.get('/search', SearchController.index);
